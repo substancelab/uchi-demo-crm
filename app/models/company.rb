54130f7e9b49796c476258a89e119fe7f64296d7
Jakob Skjerning <jakob@mentalized.net>
2025-12-01 16:07:36 +0100

    Associate people with companies through roles

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,8 @@
 class Company < ApplicationRecord
   has_one_attached :logo
 
+  has_many :roles
+  has_many :people, through: :roles
+
   validates :name, presence: true
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ApplicationRecord
+  has_many :roles
+  has_many :companies, through: :roles
 end
