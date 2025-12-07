9caa48b7fa80864bf77b4f4dff7e8dc8b696ee7f
Jakob Skjerning <jakob@mentalized.net>
2025-12-07 16:45:14 +0100

    Generate authentication system
        $ rails generate authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_01_151416) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_07_154316) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -81,9 +81,27 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_01_151416) do
     t.index ["person_id"], name: "index_roles_on_person_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "contacts", "leads"
   add_foreign_key "roles", "companies"
   add_foreign_key "roles", "people"
+  add_foreign_key "sessions", "users"
 end
