9fc848a20a625d787f25a331f18c74211747c187
Jakob Skjerning <jakob@mentalized.net>
2025-12-08 22:46:52 +0100

    Set dark mode based on the users preferences

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.documentElement.classList.add("dark")
+}
