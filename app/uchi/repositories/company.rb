f32510325a34bfe68827b8b321bf327924502c0f
Jakob Skjerning <jakob@mentalized.net>
2025-12-10 09:14:47 +0100

    Add an Action to Companies

--- app/uchi/repositories/company.rb
+++ app/uchi/repositories/company.rb
@@ -3,6 +3,12 @@
 module Uchi
   module Repositories
     class Company < Repository
+      def actions
+        [
+          Uchi::Actions::Companies::GenerateTagline.new
+        ]
+      end
+
       def fields
         [
           Field::Id.new(:id),
