848533e35079d6e8eb1b0740e3c90aacc814d1d1
Jakob Skjerning <jakob@mentalized.net>
2025-12-24 13:24:55 +0100

    Demo the sortable collections for BelongsTo fields

--- app/uchi/repositories/role.rb
+++ app/uchi/repositories/role.rb
@@ -6,11 +6,17 @@ module Uchi
       def fields
         [
           Field::BelongsTo.new(:person)
+            .collection_query(lambda { |query|
+              query.reorder(:first_name, :last_names)
+            })
             .sortable(lambda { |query, direction|
               query.joins(:person).order(person: { first_name: direction, last_names: direction })
             }),
           Field::String.new(:title),
           Field::BelongsTo.new(:company)
+            .collection_query(lambda { |query|
+              query.reorder(:name)
+            })
             .sortable(lambda { |query, direction|
               query.joins(:company).order(company: { name: direction })
             })
